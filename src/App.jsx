a646914013ef77ce4963e7a4d7e557b34cacaa48
Samir <sobiroff23@gmail.com>
2024-04-20 19:31:42 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import Layout from './layout/Layout'
+import PlayList from './playlist/playlist'
 
 function App() {
 
     return (
+        <>
+        
         <Layout>
             <section className='w-full  flex items-center justify-center ' >
                 <div className='w-20 h-20 bg-red-400' >
@@ -11,6 +14,10 @@ function App() {
                 </div>
             </section>
         </Layout>
+
+<PlayList />
+        
+        </>
     )
 }
 
---
+++ src/playlist/playlist.jsx
@@ -0,0 +1,8 @@
+
+
+
+export default function PlayList() {
+    return (
+        <h1>hi</h1>
+    )
+}

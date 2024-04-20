c6c2bbcf95f1c7f70c2b3aa523fbdcacd6868cbe
Samir <sobiroff23@gmail.com>
2024-04-20 19:36:32 +0500

    fixed

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -4,7 +4,7 @@ import { TiArrowSortedDown } from "react-icons/ti";
 export default function ProfileMenu() {
 
     return (
-        <div className="cursor-pointer flex items-center gap-2 bg-[#0A0A0A] rounded-3xl p-0.5 text-white select-none" >
+        <div className="mr-10 cursor-pointer flex items-center gap-2 bg-[#0A0A0A] rounded-3xl p-0.5 text-white select-none" >
             <img className="rounded-full object-cover" src="/images/user.png" alt="avatar" />
             <span className="" >User</span>
             <button>

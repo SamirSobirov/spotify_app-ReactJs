8a5e6f8b16b14600595879be40252a5ba5d27837
Samir <sobiroff23@gmail.com>
2024-04-20 19:49:37 +0500

    fixed

--- src/playlist/playlist.jsx
+++ src/playlist/playlist.jsx
@@ -4,8 +4,8 @@
 export default function PlayList() {
     return (
 
-        <div>
-        <h1 className="items-center">hi</h1>
+        <div className="items-center ">
+        <p className="text-center text-4xl font-bold mr-[500px] mt-[25px]">Good morning</p>
         <h2>hello World</h2>
         </div>
     )

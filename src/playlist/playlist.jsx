dd9c1b68180d1e87aae13f63228922a5765138f4
Samir <sobiroff23@gmail.com>
2024-04-22 00:51:05 +0500

    fixed

--- src/playlist/playlist.jsx
+++ src/playlist/playlist.jsx
@@ -1,49 +1,36 @@
 export default function PlayList() {
   return (
     <>
-<div className="ml-[17%]">
+      <div className="ml-[17%]">
+        <div className="items-end flex justify-center  text-center mt-[20px] ">
+          <div className="bg-[#303030] w-[20%] flex ">
+            <img className="h-20" src="./images/liked_songs.svg" alt="" />
+            <span className="p-6">Liked Songs</span>
+          </div>
 
-      <div className="items-end flex justify-center  text-center mt-[20px] ">
-        <div className="bg-white w-[20%] flex ">
-          <img className="h-20" src="./images/liked_songs.svg" alt="" />
-          <span className="p-6">Liked Songs</span>
+          <div className="bg-[#303030] w-[20%] flex ml-6">
+            <img className="h-20" src="./images/liked_songs2.svg" alt="" />
+            <span className="p-6">Liked Songs</span>
+          </div>
+
+          <div className="bg-[#303030] w-[20%] ml-6 flex ">
+            <img className="h-20" src="./images/liked_songs3.svg" alt="" />
+            <span className="p-6">Liked Songs</span>
+          </div>
+
+          <div className="bg-[#303030] w-[20%] ml-6 flex ">
+            <img className="h-20" src="./images/liked_songs.svg" alt="" />
+            <span className="p-6">Liked Songs</span>
+          </div>
         </div>
 
-
-
-
-        <div className="bg-white w-[20%] flex ml-6">
-          <img className="h-20" src="./images/liked_songs.svg" alt="" />
-          <span className="p-6">Liked Songs</span>
-        </div>
-
-
-
-
-
-
-        <div className="bg-white w-[20%] ml-6 flex ">
-          <img className="h-20" src="./images/liked_songs.svg" alt="" />
-          <span className="p-6">Liked Songs</span>
-        </div>
-
-
-
-
-        <div className="bg-white w-[20%] ml-6 flex ">
-          <img className="h-20" src="./images/liked_songs.svg" alt="" />
+        <div className="bg-[#303030] w-[20%] ml-[83px] flex mt-[20px]">
+          <img className="h-20" src="./images/liked_songs3.svg" alt="" />
           <span className="p-6">Liked Songs</span>
         </div>
       </div>
-
-
 
       
-
-
-
-
-      </div>
     </>
   );
 }

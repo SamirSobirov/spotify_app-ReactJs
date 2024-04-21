792df995ff94ef38901745b67d97a7cd1991d5e4
Samir <sobiroff23@gmail.com>
2024-04-22 01:21:31 +0500

    fixed

--- src/artistplaylist/artist.jsx
+++ src/artistplaylist/artist.jsx
@@ -6,40 +6,40 @@ export default function ArtistsPlaylist() {
     return(
 
         <>
-        <div className="items-end flex justify-center  text-center mt-[20px] gap-12 ml-[280px]">
+        <div className="items-end flex justify-center  text-center mt-[40px] gap-12 ml-[280px]">
 
-            <div className="w-32">
+            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-[15px] text-white text-start">Weekly Motivatio...</p>
+                <p className="pt-[10px] text-[15px] text-white text-start">Weekly Motivatio...</p>
             </div>
 
 
-            <div className="w-32">
+            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-[15px] text-white text-start">MEDITATION SELF</p>
+                <p className="pt-[10px] text-[15px] text-white text-start">MEDITATION SELF</p>
             </div>
 
 
-            <div className="w-32">
+            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-[14px] text-white text-start">Words beyond act...</p>
+                <p className="pt-[10px] text-[14px] text-white text-start">Words beyond act...</p>
             </div>
 
-            <div className="w-32">
+            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-[15px] text-white text-start">The Alexa Show</p>
-            </div>
-
-
-            <div className="w-32">
-                <img src="./images/artist1.svg" alt="" />
-                <p className="text-[15px] text-white text-start">The Stories of Ma...</p>
+                <p className="pt-[10px] text-[15px] text-white text-start">The Alexa Show</p>
             </div>
 
 
-            <div className="w-32">
+            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-[14px] text-white text-start">Motivation Daily b...</p>
+                <p className="pt-[10px] text-[15px] text-white text-start">The Stories of Ma...</p>
+            </div>
+
+
+            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
+                <img src="./images/artist1.svg" alt="" />
+                <p className="pt-[10px] text-[14px] text-white text-start">Motivation Daily b...</p>
             </div>
             
             

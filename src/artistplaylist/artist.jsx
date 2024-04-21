9d98921eb5545ad0aed009b01c3d247adf3a4774
Samir <sobiroff23@gmail.com>
2024-04-22 01:16:15 +0500

    fixed

--- src/artistplaylist/artist.jsx
+++ src/artistplaylist/artist.jsx
@@ -10,36 +10,36 @@ export default function ArtistsPlaylist() {
 
             <div className="w-32">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-lg text-white text-start">Weekly Motivatio...</p>
+                <p className="text-[15px] text-white text-start">Weekly Motivatio...</p>
             </div>
 
 
             <div className="w-32">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-lg text-white text-start">Weekly Motivatio...</p>
+                <p className="text-[15px] text-white text-start">MEDITATION SELF</p>
             </div>
 
 
             <div className="w-32">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-lg text-white text-start">Weekly Motivatio...</p>
+                <p className="text-[14px] text-white text-start">Words beyond act...</p>
             </div>
 
             <div className="w-32">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-lg text-white text-start">Weekly Motivatio...</p>
-            </div>
-
-
-            <div className="w-32">
-                <img src="./images/artist1.svg" alt="" />
-                <p className="text-lg text-white text-start">Weekly Motivatio...</p>
+                <p className="text-[15px] text-white text-start">The Alexa Show</p>
             </div>
 
 
             <div className="w-32">
                 <img src="./images/artist1.svg" alt="" />
-                <p className="text-lg text-white text-start">Weekly Motivatio...</p>
+                <p className="text-[15px] text-white text-start">The Stories of Ma...</p>
+            </div>
+
+
+            <div className="w-32">
+                <img src="./images/artist1.svg" alt="" />
+                <p className="text-[14px] text-white text-start">Motivation Daily b...</p>
             </div>
             
             

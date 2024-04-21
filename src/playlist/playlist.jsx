0ae054bb6a8ceea44302c72c0471e0b2c5dd9877
Samir <sobiroff23@gmail.com>
2024-04-22 01:28:58 +0500

    fixed

--- src/playlist/playlist.jsx
+++ src/playlist/playlist.jsx
@@ -5,28 +5,28 @@ export default function PlayList() {
         <div className="items-end flex justify-center  text-center mt-[20px] ">
           <div className="bg-[#303030] w-[20%] flex ">
             <img className="h-20" src="./images/liked_songs.svg" alt="" />
-            <span className="p-6">Liked Songs</span>
+            <span className="p-6 text-white">Liked Songs</span>
           </div>
 
           <div className="bg-[#303030] w-[20%] flex ml-6">
             <img className="h-20" src="./images/liked_songs2.svg" alt="" />
-            <span className="p-6">Liked Songs</span>
+            <span className="p-6 text-white">Neffex Playlist</span>
           </div>
 
           <div className="bg-[#303030] w-[20%] ml-6 flex ">
             <img className="h-20" src="./images/liked_songs3.svg" alt="" />
-            <span className="p-6">Liked Songs</span>
+            <span className="p-6 text-white">K/ DA</span>
           </div>
 
           <div className="bg-[#303030] w-[20%] ml-6 flex ">
             <img className="h-20" src="./images/liked_songs.svg" alt="" />
-            <span className="p-6">Liked Songs</span>
+            <span className="p-6 text-white">Liked Songs</span>
           </div>
         </div>
 
         <div className="bg-[#303030] w-[20%] ml-[83px] flex mt-[20px]">
           <img className="h-20" src="./images/liked_songs3.svg" alt="" />
-          <span className="p-6">Liked Songs</span>
+          <span className="p-6 text-white">Liked Songs</span>
         </div>
       </div>
 

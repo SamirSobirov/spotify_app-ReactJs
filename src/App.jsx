0d83be231ca4684d49ea41562fecaea9ba87c65c
Samir <sobiroff23@gmail.com>
2024-04-22 22:14:45 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,12 @@ function App() {
 
       <PlayList />
 
-      <div className="items-center ">
-          <p className="text-center text-4xl font-bold mr-[420px] mt-[48px] text-white">
+      <div className="items-center flex justify-between whitespace-nowrap">
+          <p className="text-center text-4xl font-bold ml-[330px] mt-[48px] text-white">
           Shows you might like
           </p>
+
+          <a href="#" className="mr-[100px] text-white mt-14">SEE ALL</a>
         </div>
 
 
--- src/artistplaylist/artist.jsx
+++ src/artistplaylist/artist.jsx
@@ -6,40 +6,52 @@ export default function ArtistsPlaylist() {
     return(
 
         <>
-        <div className="items-end flex justify-center  text-center mt-[40px] gap-12 ml-[250px]">
+        <div className="w-[100%] items-end flex justify-center  text-center mt-[40px] gap-[28px] ml-[120px]">
 
-            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
-                <img src="./images/artist1.svg" alt="" />
-                <p className="pt-[10px] text-[15px] text-white text-start">Weekly Motivatio...</p>
+            <div className="w-[150px] bg-[#181818] h-[180px] rounded-lg">
+                <div className="items-center text-center pt-[10px] pl-[15px] ">
+                <img className="w-[120px]" src="./images/artist1.svg" alt="" />
+                <p className="pt-[10px] text-[15px] text-white text-start ">Weekly Motivatio...</p>
+                </div>
             </div>
 
 
-            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
-                <img src="./images/artist2.svg" alt="" />
+            <div className="w-[150px] bg-[#181818] h-[180px] rounded-lg">
+            <div className="items-center text-center pt-[10px] pl-[15px] ">
+            <img className="w-[120px]" src="./images/artist2.svg" alt="" />
                 <p className="pt-[10px] text-[15px] text-white text-start">MEDITATION SELF</p>
+                </div>
             </div>
 
 
-            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
-                <img src="./images/artist3.svg" alt="" />
+            <div className="w-[150px] bg-[#181818] h-[180px] rounded-lg">
+            <div className="items-center text-center pt-[10px] pl-[15px] ">
+            <img className="w-[120px]" src="./images/artist3.svg" alt="" />
                 <p className="pt-[10px] text-[14px] text-white text-start">Words beyond act...</p>
+                </div>
             </div>
 
-            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
-                <img src="./images/artist4.svg" alt="" />
+            <div className="w-[150px] bg-[#181818] h-[180px] rounded-lg">
+            <div className="items-center text-center pt-[10px] pl-[15px] ">
+            <img className="w-[120px]" src="./images/artist4.svg" alt="" />
                 <p className="pt-[10px] text-[15px] text-white text-start">The Alexa Show</p>
+                </div>
             </div>
 
 
-            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
-                <img src="./images/artist5.svg" alt="" />
+            <div className="w-[150px] bg-[#181818] h-[180px] rounded-lg">
+            <div className="items-center text-center pt-[10px] pl-[15px] ">
+            <img className="w-[120px]" src="./images/artist5.svg" alt="" />
                 <p className="pt-[10px] text-[15px] text-white text-start">The Stories of Ma...</p>
+                </div>
             </div>
 
 
-            <div className="w-32 bg-[#181818] h-[180px] rounded-lg">
-                <img src="./images/artist6.svg" alt="" />
+            <div className="w-[150px] bg-[#181818] h-[180px] rounded-lg">
+            <div className="items-center text-center pt-[10px] pl-[15px] ">
+            <img className="w-[120px]" src="./images/artist6.svg" alt="" />
                 <p className="pt-[10px] text-[14px] text-white text-start">Motivation Daily b...</p>
+                </div>
             </div>
             
             

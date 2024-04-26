f07a7fb90d66cf9694dd37db62e8f77e977f6a5c
Samir <sobiroff23@gmail.com>
2024-04-26 14:48:48 +0500

    fixed

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,7 +5,7 @@ export default function Search() {
   return (
     <>
 
-<div className="items-start text-left pl-[350px]">
+<div className="items-start text-left pl-[335px]">
 <form class="form relative ">
   <button class="absolute left-2 -translate-y-1/2 top-1/2 p-1">
     <svg
@@ -27,26 +27,11 @@ export default function Search() {
     </svg>
   </button>
   <input
-    class="input rounded-full px-8 py-3 border-2 border-transparent focus:outline-none focus:border-blue-500 placeholder-gray-400 transition-all duration-300 shadow-md"
-    placeholder="Search..."
+    class="input w-[455px] rounded-full px-8 py-3 border-2 border-transparent focus:outline-none focus:border-blue-500 placeholder-gray-400 transition-all duration-300 shadow-md"
+    placeholder="Artists, songs, or podcasts"
     required=""
     type="text"
   />
-  <button type="reset" class="absolute right-3 -translate-y-1/2 top-1/2 p-1">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="w-5 h-5 text-gray-700"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M6 18L18 6M6 6l12 12"
-      ></path>
-    </svg>
-  </button>
 </form>
 </div>
 

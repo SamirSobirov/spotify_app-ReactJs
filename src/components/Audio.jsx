1d20a7dd9f156b970df4b2451a5564df4f263c43
Samir <sobiroff23@gmail.com>
2024-05-01 23:07:15 +0500

    fixed

---
+++ src/components/Audio.jsx
@@ -0,0 +1,19 @@
+
+import { Audio } from 'react-loader-spinner'
+
+
+export default function AudioSpinner() {
+    return (
+        <Audio
+            height="20"
+            width="20"
+            color="#4fa94d"
+            
+            // ariaLabel="audio-loading"
+            // wrapperStyle={{}}
+            // wrapperClass="wrapper-class"
+            
+            visible={true}
+        />
+    )
+}
--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,18 +1,22 @@
+
 import { useContext } from "react";
 import { TrackContext } from "../context/TrackCTX";
+import AudioSpinner from "./Audio";
 
 
-function Track({src, img, name, singers, album, date, duration, index}) {
-    const {track, setTrack} = useContext(TrackContext)
+function Track({ src, img, name, singers, album, date, duration, index }) {
+    const { track, setTrack } = useContext(TrackContext)
 
-    return ( 
-        <div 
-            onClick={() => setTrack({img, name, singers, album, date, duration, index, src})}
+    return (
+        <div
+            onClick={() => setTrack({ img, name, singers, album, date, duration, index, src })}
             className="track grid grid-cols-5 items-center px-5 py-2 rounded cursor-pointer hover:bg-[#2a2a2a]"
         >
-            {}
             <div className="flex items-center gap-5 col-start-1 col-end-3">
-                <span className="text-lg">{index + 1}</span>
+                {
+                    index === track?.index ? <AudioSpinner size={13}/> :
+                        <span className="text-lg">{index + 1}</span>
+                }
                 <img className="w-11 h-11 rounded" src={img} alt={name} />
                 <div>
                     <h4 className="text-white cursor-pointer hover:underline">{name}</h4>
@@ -24,7 +28,7 @@ function Track({src, img, name, singers, album, date, duration, index}) {
             <h4>{date}</h4>
             <h4 className="flex justify-center">{duration}</h4>
         </div>
-     );
+    );
 }
 
 export default Track;

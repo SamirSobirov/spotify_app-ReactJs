2ecad8663156a86eccfcf1a3b9dc5c097cc2fb32
Samir <sobiroff23@gmail.com>
2024-05-02 19:51:27 +0500

    fixed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -48,7 +48,7 @@ export default function Player(params) {
         ) : (
           <img
             className="w-[70px] h-[70px]"
-            src="https://plus.unsplash.com/premium_photo-1676834376020-a17554ed4fbb?q=80&w=2097&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="/images/liked_songs1.svg"
             alt=""
           />
         )}

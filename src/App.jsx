798d2780f8893cb1f7181014977bd17f599931e7
Sobirov S <sobiroff23@gmail.com>
2024-06-28 23:30:14 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,8 @@ function App() {
     const [track, setTrack] = useState(null)
     const [playlist_ctx, setPLaylist_ctx] = useState([])
 
-
-
-    
     return (
-        <PLaylistContext.Provider value={{playlist_ctx, setPLaylist_ctx}} >
+        <PLaylistContext.Provider value={{ playlist_ctx, setPLaylist_ctx }} >
             <TrackContext.Provider value={{ track, setTrack }} >
                 <Routes>
                     <Route path='/' element={<Layout />}>
